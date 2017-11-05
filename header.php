f86cef31f1153fbcca2b2831eec492e4b360ca3d
Student <Student>
2017-11-05 12:19:24 +0000

    header with box-sizing

--- header.php
+++ header.php
@@ -1,22 +1,36 @@
 <header>
-    <div class="header" id="logo-pic" name="Logo"> 
-    <img src="img/logo.png" id="logo"/>
+    <div class="header">
+        <div class="row">
+            <div class="col-2">
+                <div  id="logo-pic" name="Logo">
+                        <img src="img/logo.png" id="logo">
+                </div>
+                <div id="logo-text" name="Logo Text">
+                        <p>Relocate Right</p>
+                </div>
+             </div>
+            <div class ="col-9">
+                <div id="brand" name="Brand">
+                    <h2>
+                        Relocate Right is an estate agency which has been providing services for buying,
+                     selling and renting properties in Central London and surrounding areas for over a decade.
+                     </h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <label class="col-2" for="left-menu"></label>
+            <div class="col-9">
+                 <div id ="left-menu" name="Menu">
+                        <ul>
+                            <li>Home</li>
+                            <li>Buy</li>
+                            <li>Rent</li>
+                            <li>Contact</li>
+                            <li>About</li>
+                        </ul>
+                  </div>
+            </div>
+        </div>
     </div>
-    <div class="header" id="brand" name="Brand"> 
-        <h2>
-                Relocate Right is an estate agency which has been providing services for buying, 
-             selling and renting properties in Central London and surrounding areas for over a decade. 
-        </h2>
-    </div>
-    
-    <div class="header" id ="left-menu" name="Menu">
-    <ul>
-        <li>Home</li>
-        <li>Buy</li>
-        <li>Sell</li>
-        <li>Rent</li>
-        <li>Contact</li>
-        <li>About</li>
-    </ul>
-    </div>        
 </header>
