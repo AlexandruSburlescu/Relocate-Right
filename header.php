4fa3b5491a23911dd07b717497249efcc54b37bd
Student <Student>
2017-11-05 14:57:50 +0000

    new styles added

--- header.php
+++ header.php
@@ -2,31 +2,35 @@
     <div class="header">
         <div class="row">
             <div class="col-2">
-                <div  id="logo-pic" name="Logo">
+               <div class="row">
+                   <div  id="logo-pic" name="Logo">
                         <img src="img/logo.png" id="logo">
-                </div>
+                   </div>
                 <div id="logo-text" name="Logo Text">Relocate Right</div>
              </div>
+            </div>
             <div class ="col-9">
                 <div id="brand" name="Brand">
-                  Relocate Right is an estate agency which has been providing services for buying,
+                    <h2> Relocate Right</h2 \n>
+                    is an estate agency which has been providing services for buying,
                     selling and renting properties in Central London and surrounding areas for over a decade.
+                </div>
+                <div class="row">
+                    <label for="left-menu"></label>
+                    <div class="col-12">
+                        <div id ="left-menu" name="Menu">
+                            <ul>
+                                <li><a href="index.php" class="button">Home</a></li>
+                                <li><a href="listings/buy.php" class="button">Buy</a></li>
+                                <li><a href="listings/rent.php" class="button">Rent</a></li>
+                                <li><a href="contact.php" class="button">Contact</a></li>
+                                <li><a href="about.php" class="button">About</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <label class="col-2" for="left-menu"></label>
-            <div class="col-9">
-                 <div id ="left-menu" name="Menu">
-                        <ul>
-                            <li class="button"><a href="index.php">Home</a></li>
-                            <li class="button"><a href="listings/buy.php">Buy</a></li>
-                            <li class="button"><a href="listings/rent.php">Rent</a></li>
-                            <li class="button"><a href="contact.php">Contact</a></li>
-                            <li class="button"><a href="about.php">About</a></li>
-                        </ul>
-                  </div>
-            </div>
-        </div>
+
     </div>
 </header>
--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
 <head>
 <title>Relocate Right - Estate Agency</title>
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width"/>
+
 <meta charset="utf-8" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/main.css" />
+<link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Open+Sans|Roboto" rel="stylesheet">
 <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
 </head>
 <body>
