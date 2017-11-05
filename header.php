164be935270d341c4b234ce82196bd3330e2a11b
Student <Student>
2017-11-05 12:59:25 +0000

    header with box-sizing

--- header.php
+++ header.php
@@ -5,16 +5,12 @@
                 <div  id="logo-pic" name="Logo">
                         <img src="img/logo.png" id="logo">
                 </div>
-                <div id="logo-text" name="Logo Text">
-                        <p>Relocate Right</p>
-                </div>
+                <div id="logo-text" name="Logo Text">Relocate Right</div>
              </div>
             <div class ="col-9">
                 <div id="brand" name="Brand">
-                    <h2>
-                        Relocate Right is an estate agency which has been providing services for buying,
-                     selling and renting properties in Central London and surrounding areas for over a decade.
-                     </h2>
+                  Relocate Right is an estate agency which has been providing services for buying,
+                    selling and renting properties in Central London and surrounding areas for over a decade.
                 </div>
             </div>
         </div>
@@ -23,11 +19,11 @@
             <div class="col-9">
                  <div id ="left-menu" name="Menu">
                         <ul>
-                            <li>Home</li>
-                            <li>Buy</li>
-                            <li>Rent</li>
-                            <li>Contact</li>
-                            <li>About</li>
+                            <li class="button"><a href="index.php">Home</a></li>
+                            <li class="button"><a href="listings/buy.php">Buy</a></li>
+                            <li class="button"><a href="listings/rent.php">Rent</a></li>
+                            <li class="button"><a href="contact.php">Contact</a></li>
+                            <li class="button"><a href="about.php">About</a></li>
                         </ul>
                   </div>
             </div>
